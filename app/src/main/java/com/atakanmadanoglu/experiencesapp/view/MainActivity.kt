db767fafdac86f2eb133bbfe86ead4e499035593
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-05-20 22:52:58 +0300

    Designed login page

--- app/src/main/java/com/atakanmadanoglu/experiencesapp/view/MainActivity.kt
+++ app/src/main/java/com/atakanmadanoglu/experiencesapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.atakanmadanoglu.experiencesapp
+package com.atakanmadanoglu.experiencesapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.atakanmadanoglu.experiencesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
